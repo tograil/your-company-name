760ef92c0ba2b1ce51a46001ed35852367e4a09e
CDNDR <skritkaz@gmail.com>
2016-07-22 18:08:10 +0600

    add chart in front-end, fix folder (hate bower ;c), add logic for chart

--- app/app.js
+++ app/app.js
@@ -1,4 +1,38 @@
-var app = angular.module('app', ['angular-ellipses', 'ui.router', 'LocalStorageModule', 'dataServiceModule']);
+var app = angular.module('app', ['angular-ellipses', 'ui.router', 'LocalStorageModule', 'dataServiceModule', 'chart.js'])
+
+// Optional configuration
+  .config(['ChartJsProvider', function(ChartJsProvider) {
+    // Configure all charts
+    ChartJsProvider.setOptions({
+      chartColors: ['#FF5252', '#FF8A80'],
+      responsive: false
+    });
+    // Configure all line charts
+    ChartJsProvider.setOptions('line', {
+      showLines: false
+    });
+  }])
+  .controller("LineCtrl", ['$scope', '$timeout', function($scope, $timeout) {
+
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+    $scope.onClick = function(points, evt) {
+      console.log(points, evt);
+    };
+
+    // Simulate async data update
+    $timeout(function() {
+      $scope.data = [
+        [28, 48, 40, 19, 86, 27, 90],
+        [65, 59, 80, 81, 56, 55, 40]
+      ];
+    }, 3000);
+  }]);
+
 
 app.constant('globalConstants', {
   apiUrl: "http://localhost:51925/",
@@ -23,4 +57,3 @@ app.run(function($rootScope, $templateCache) {
     $templateCache.removeAll();
   });
 });
-
