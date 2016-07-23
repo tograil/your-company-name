a11a32cbb80292e4ca339d277959142b9cf83e0e
Alexander V <alexvab@gmail.com>
2016-07-23 21:16:23 +0300

    changed chart width

--- app/app.js
+++ app/app.js
@@ -20,6 +20,16 @@ var app = angular.module('app', ['angular-ellipses', 'ui.router', 'LocalStorageM
       [65, 59, 80, 81, 56, 55, 40],
       [28, 48, 40, 19, 86, 27, 90]
     ];
+
+     /*$scope.options = [
+         {
+            size: {
+               height: 200,
+               width: 700
+            }
+         }
+       ];
+    $scope.heightChart = window.innerHeight*0.7;*/
     $scope.onClick = function(points, evt) {
       console.log(points, evt);
     };
