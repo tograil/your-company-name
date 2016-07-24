6bbb65abc37dfa35c4f9ffee700abe97f1485122
Andrey Rubayko <tograilx@gmail.com>
2016-07-24 12:10:38 +0300

    show data

--- app/controllers/datasettingsController.js
+++ app/controllers/datasettingsController.js
@@ -4,6 +4,9 @@ var app = angular.module('app');
 app.controller('DataSettings', ['$scope', '$state', '$stateParams', 'dataService',
   function ($scope, $state, $stateParams, dataService) {
 
+
+
+
   $scope.subject = [
     {desc: '01 - GENERAL MOBILIZATION & DEMOBILIZATION - Structure ‏1'},
     {desc: '02 - CHAPTER 01 - Main Breakwater Extension - MBE'},
@@ -21,40 +24,41 @@ app.controller('DataSettings', ['$scope', '$state', '$stateParams', 'dataService
     {desc: '14 - TOTAL By Months'}
   ];
 
-  $scope.accumulate = {
-    titlePlan: 'Accumulate Plan',
-    Plan: [
-      {month: '9 367'},
-      {month: '28 365'},
-      {month: '47 479'},
-      {month: '59 622'},
-      {month: '75 499'},
-      {month: '123 246'},
-      {month: '163 177'},
-      {month: '225 025'},
-      {month: '271 650'},
-      {month: '365 930'},
-      {month: '503 338'},
-      {month: '621 474'}
-    ],
-    titleActual: 'Accumulate Actual',
-    Actual: [
-      {month: '9 367'},
-      {month: '28 352'},
-      {month: '47 470'},
-      {month: '59 613'},
-      {month: '75 491'},
-      {month: '87 145'},
-      {month: '117 366'},
-      {month: '169 999'},
-      {month: '197 712'},
-      {month: '221 530'},
-      {month: '293 411'},
-      {month: ''}
-    ]
-  };
+    $scope.selectedItem = $scope.subject[0];
+  $scope.plan = [
+    {
+      title: 'Plan',
+      data: [ '9 367' , '28 365' , '47 479', '59 622', '75 499', '123 246', '163 177', '225 025', '271 650', '365 930', '503 338', '621 474' ],
+      index: 0
+    } ,
+    {
+      title: 'AccumulatedPlan',
+      data: [ '9 367' , '28 365ss' , '47 479sss', '59 622', '75 499', '123 246', '163 177', '225 025', '271 650', '365 930', '503 338', '621 474' ],
+      index: 1
+
+    }
+  ];
+
+
+    $scope.actual = [
+      {
+        title: 'Plan',
+        data: [ '9 367' , '28 365' , '47 479', '59 622', '75 499', '123 246', '163 177', '225 025', '271 650', '365 930', '503 338', '621 474' ],
+        index: 0,
+      } ,
+      {
+        title: 'AccumulatedPlan',
+        data: [ '9 367' , '28 365ss' , '47 479sss', '59 622', '75 499', '123 246', '163 177', '225 025', '271 650', '365 930', '503 338', '621 474' ],
+        index: 1
+      }
+    ];
+
+    $scope.planIndex = $scope.plan[0];
+    $scope.actualIndex = $scope.actual[0];
 
   $scope.colors = ['red', 'orange', 'yellow', 'green', 'blue', 'pink'];
+  $scope.colorAccPlan = 'red';
+  $scope.colorAccActual = 'orange';
 
   $scope.returnClass = function (colorModel) {
     return colorModel;
@@ -62,25 +66,96 @@ app.controller('DataSettings', ['$scope', '$state', '$stateParams', 'dataService
     $scope.data = {};
 
 
+    function getData(items) {
+      var data = [];
+      for(var i=0; i<items.length; i++)
+      {
+        data.push({
+          index:i,
+          title: items[i].title,
+          data: items[i].data
+        });
+      }
+
+      return data;
+    }
+
 
     dataService.getDocumentPlanData($stateParams.documentId).success(function (data) {
       $scope.data = data;
 
       $scope.subject = [];
 
-      for(var i=3; i<data.planItems.length; i++)
+      for(var i=0; i<data.settingItems.length; i++)
       {
         $scope.subject.push({
           index: i,
-          desc: data.planItems[i].subject
+          desc: data.settingItems[i].subject,
+          plan: getData(data.settingItems[i].planDataItems),
+          actual: getData(data.settingItems[i].actualDataItems)
         });
       }
 
-      $scope.selectedItem = $scope.subject[0];
+      $scope.years = $scope.data.years;
+      $scope.yearsGrouped = groupYears($scope.data.years);
+      $scope.monthes = $scope.data.monthes;
+
+      $scope.labels = returnLabels($scope.monthes, $scope.years )
     });
-    
+
+    function returnLabels(monthes, years) {
+      var labels = [];
+
+      for (var i=0; i < monthes.length; i++)
+      {
+        labels.push(monthes[i] + " " + years[i]);
+      }
+
+      return labels;
+    }
+
+    $scope.years = ['2016', '2016'];
+    $scope.yearsGrouped = {};
+    $scope.yearsKeys = [];
+    $scope.monthes = ['Jan', 'Feb'];
+
+    function groupYears(years) {
+      var grouped = {};
+      var hist = {};
+      $scope.yearsKeys = [];
+      years.map( function (a) { if (a in hist) hist[a] ++; else
+      {
+        hist[a] = 1;
+        $scope.yearsKeys.push(a);
+      } } );
+
+      return hist;
+    }
+
     $scope.changed = function () {
-      
+      if($scope.selectedItem == null)
+          return;
+
+      var selectedItem = $scope.selectedItem;
+
+      $scope.plan = selectedItem.plan;
+      $scope.actual = selectedItem.actual;
+
+      redrawChart(selectedItem.plan, selectedItem.actual);
     };
+
+    function redrawChart(plan, actual) {
+      $scope.series = [ plan.title, actual.title ];
+      $scope.data = [ plan.data, actual.data ];
+    }
+
+    $scope.redrawChart = function () {
+      $scope.series = [ $scope.planIndex.title, $scope.actualIndex.title ];
+      $scope.data = [ $scope.planIndex.data, $scope.actualIndex.data ];
+    }
+
+    $scope.labels = [];
+    $scope.series = [];
+    $scope.data = [];
 
 }]);
