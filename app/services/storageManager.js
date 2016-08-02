cf7ef781164ce7d6b9d12e6da9f5c12cfd299e2c
Dmytro Petlia <dima.petlia@hotmail.com>
2016-08-03 02:42:41 +0300

    added proper authentication mechanism. Fix dashboard dropdown

---
+++ app/services/storageManager.js
@@ -0,0 +1,40 @@
+﻿(function () {
+
+    "use strict";
+
+    angular
+        .module("uvsensor.utils")
+        .service("uvUtils.storageManager", storageManager);
+
+    /**
+     * Declare storage service to manage localDB & cookies.
+     * @param {"utils.cookieService"} cookieService
+     * @param {"utils.localDBService"} localDBService
+     */
+    function storageManager(cookieService, localDBService) {
+
+        var _sm = this,
+            TYPE = {
+                "cookie": cookieService,
+                "localDB": localDBService
+            };
+
+        // Forcibly assign a new storage type.
+        this.specify = function (newType) {
+            for (var prop in TYPE[newType]) {
+                if (TYPE[newType].hasOwnProperty(prop)) {
+                    _sm[prop] = TYPE[newType][prop];
+                }
+            }
+            return _sm;
+        };
+
+    }
+
+    // IoC container.
+    storageManager.$inject = [
+        "uvUtils.cookieService",
+        "uvUtils.localDBService"
+    ];
+
+})();
